83158bdaa3463a2e3f693548f61ca55c21023359
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2025-03-06 11:21:12 +0100

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,29 +1,43 @@
 import sqlite3
-from datetime import datetime
+import os
 
-DB_PATH = "logs/ssh_logs.db"
+DB_DIR = "logs"
+DB_PATH = os.path.join(DB_DIR, "ssh_logs.db")
 
 def init_db():
+    # Vérifier si le dossier logs existe, sinon le créer
+    if not os.path.exists(DB_DIR):
+        os.makedirs(DB_DIR)
+
+    # Connexion à la base de données (elle est créée si elle n'existe pas)
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS ssh_logs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ip TEXT,
+            ip TEXT NOT NULL,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            command TEXT
+            command TEXT NOT NULL
         )
     ''')
+
     conn.commit()
     conn.close()
 
 def log_ssh_attempt(ip, command):
+    # Vérifie si la base de données existe, sinon l'initialiser
+    if not os.path.exists(DB_PATH):
+        init_db()
+
+    # Insére
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute('''
         INSERT INTO ssh_logs (ip, command)
         VALUES (?, ?)
     ''', (ip, command))
+
     conn.commit()
     conn.close()
 
